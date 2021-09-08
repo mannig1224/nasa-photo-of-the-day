4be79c6609b8026484b3e7065e7dbf836bc822e3
ManniG24 <egatica51@gmail.com>
2021-09-08 15:15:49 -0600

    api request to nasa api finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
+import {apiKey, url} from './config';
 
 function App() {
+  const [nasaData, setNasaData] = useState();
+
+  useEffect(() => {
+    axios.get(`${url}api_key=${apiKey}`)
+      .then(results => {
+        console.log(results.data.explanation);
+        setNasaData(results.data);
+        
+      })
+      .catch(err => {
+        debugger
+      })
+  }, [])
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      {// Display a loading message while the data is fetching
+      (!nasaData) ? <h3>Loading...</h3> : <h2>{nasaData.title}</h2>}
+      {// Display a loading message while the data is fetching
+      (!nasaData) ? <h3>Loading...</h3> : <img src={nasaData.hdurl} alt={'stuff'}></img>}
+      {// Display a loading message while the data is fetching
+        (!nasaData) ? <h3>Loading...</h3> : <h3>{nasaData.explanation}</h3>}
+      
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const apiKey = 'f8VBhHSHLOg7Gd3u8OhqDe9pTkKKi8kLgzatft3m';
+export const url = 'https://api.nasa.gov/planetary/apod?';
